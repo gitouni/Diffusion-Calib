346889886cd34d9d6948397345bb9abda9bc2f45
gitouni <30369762+gitouni@users.noreply.github.com>
2024-08-06 22:59:59 +0100

    Add files via upload

--- models/denoiser.py
+++ models/denoiser.py
@@ -1,7 +1,11 @@
 from .tools.core import FusionNet
+from .util import se3
+from .util.seq_utils import transformer_encoder_wrapper
 import torch.nn as nn
 import torch
-from typing import Dict, Sequence
+from typing import Dict, Sequence, Tuple
+
+
 
 class Surrogate(nn.Module):
     def __init__(self, hidden_dims:Sequence[int], x_dim:int, encoder_argv:Dict) -> None:
@@ -9,10 +13,10 @@ class Surrogate(nn.Module):
         self.x_dim = x_dim
         self.encoder = FusionNet(**encoder_argv)
         mlps = [nn.Linear(self.encoder.out_dim, hidden_dims[0]),
-                nn.LeakyReLU()]
+                nn.ReLU(inplace=True)]
         for i in range(len(hidden_dims) -1):
             mlps.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
-            mlps.append(nn.LeakyReLU())
+            mlps.append(nn.ReLU(inplace=True))
         mlps.append(nn.Linear(hidden_dims[-1], x_dim))
         self.mlps = nn.Sequential(*mlps)
 
@@ -21,3 +25,53 @@ class Surrogate(nn.Module):
         x0 = self.mlps(feat)
         return x0  # (B, x_dim)
     
+class SeqSurrogate(nn.Module):
+    def __init__(self, hidden_dims:Sequence[int], x_dim:int, encoder_argv:Dict, atten_argv:Dict) -> None:
+        super().__init__()
+        self.x_dim = x_dim
+        self.encoder = FusionNet(**encoder_argv)
+        d_model = self.encoder.out_dim
+        self.atten = transformer_encoder_wrapper(d_model=d_model, **atten_argv)
+        self.reg_token = nn.Parameter(torch.rand(1, 1, d_model))
+        mlps = [nn.Linear(d_model, hidden_dims[0]),
+                nn.ReLU(inplace=True)]
+        for i in range(len(hidden_dims) -1):
+            mlps.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
+            mlps.append(nn.ReLU(inplace=True))
+        mlps.append(nn.Linear(hidden_dims[-1], x_dim))
+        self.mlps = nn.Sequential(*mlps)
+
+    def forward(self, img:torch.Tensor, pcd:torch.Tensor, camera_info:Dict):
+        B, K, C, H, W = img.shape
+        B, K, _, N = pcd.shape
+        feat = self.encoder(img.view(B*K, C, H, W), pcd.view(B*K,-1,N), camera_info)  # (B*K, D)
+        feat = self.atten(torch.cat([feat.view(B, K, -1), self.reg_token.expand(B, 1, -1)], dim=1))  # (B, K+1, D)
+        x0 = self.mlps(feat[:,-1,:])  # the final token has cross attention with other tokens
+        return x0  # (B, x_dim)
+    
+class Denoiser(nn.Module):
+    def __init__(self, model:nn.Module):
+        super().__init__()
+        self.model = model
+
+    def forward(self, x_t:torch.Tensor, x_cond:Tuple[torch.Tensor, torch.Tensor, torch.Tensor]):
+        img, pcd, camera_info = x_cond
+        se3_x_t = se3.exp(x_t)
+        pcd_tf = se3.transform(se3_x_t, pcd)  # transformed pcd
+        delta_x0 = self.model(img, pcd_tf, camera_info)
+        x0 = se3.exp(delta_x0) @ se3_x_t  # sequentially transformed by se3_x_t and x0
+        return se3.log(x0)
+    
+class SeqDenoiser(nn.Module):
+    def __init__(self, model:nn.Module):
+        super().__init__()
+        self.model = model
+
+    def forward(self, x_t:torch.Tensor, x_cond:Tuple[torch.Tensor, torch.Tensor, torch.Tensor]):
+        img, pcd, camera_info = x_cond  # (B, K, C, H, W),  (B, K, 3, N), Dict
+        K = pcd.shape[1]
+        se3_x_t = se3.exp(x_t) # (B, 4, 4)
+        pcd_tf = se3.transform(se3_x_t.unsqueeze(1).expand(-1, K, -1, -1), pcd)  # transformed pcd  (B, K, 3, N)
+        delta_x0 = self.model(img, pcd_tf, camera_info)
+        x0 = se3.exp(delta_x0) @ se3_x_t  # (B, 4, 4)
+        return se3.log(x0)  # (B, 6)
---
+++ models/diffuser.py
@@ -0,0 +1,289 @@
+import math
+import torch
+import torch.nn as nn
+from inspect import isfunction
+from functools import partial
+import numpy as np
+from tqdm import tqdm
+from typing import Union, Tuple, Literal, Iterable, Dict
+from .util.se3 import exp as se3_exp
+from .denoiser import Denoiser
+from .dpm import NoiseScheduleVP, DPM_Solver, model_wrapper
+
+def exists(x):
+	return x is not None
+
+def default(val, d):
+	if exists(val):
+		return val
+	return d() if isfunction(d) else d
+
+def extract(a:torch.Tensor, t:torch.Tensor, x_shape=(1,1,1,1)):
+	b, *_ = t.shape
+	out = a.gather(-1, t)
+	return out.reshape(b, *((1,) * (len(x_shape) - 1)))
+
+def linear_beta_schedule(timesteps):
+    scale = 1000 / timesteps
+    beta_start = scale * 0.0001
+    beta_end = scale * 0.02
+    return torch.linspace(beta_start, beta_end, timesteps, dtype=torch.float64)
+
+
+def cosine_beta_schedule(timesteps, s=0.008):
+    """
+    cosine schedule
+    as proposed in https://openreview.net/forum?id=-NEXDKk8gZ
+    """
+    steps = timesteps + 1
+    x = torch.linspace(0, timesteps, steps, dtype=torch.float64)
+    alphas_cumprod = torch.cos(((x / timesteps) + s) / (1 + s) * math.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return torch.clip(betas, 0, 0.999)
+
+def make_beta_schedule(schedule:Literal['quad','linear','warmup10','warmup50','const','jsd','cosine'],
+						n_timestep:int, linear_start:float=1e-6, linear_end:float=1e-2, cosine_s:float=8e-3):
+	"""beta schedule
+
+	Args:
+		schedule (str): Literal['quad','linear','warmup10','warmup50','const','jsd','cosine']
+		n_timestep (int): number of timesteps
+		linear_start (float, optional): Defaults to 1e-6.
+		linear_end (float, optional): Defaults to 1e-2.
+		cosine_s (float, optional): Defaults to 8e-3.
+
+	Raises:
+		NotImplementedError: schedule not in ['quad','linear','warmup10','warmup50','const','jsd','cosine']
+
+	Returns:
+		np.ndarray: (n_timestep)
+	"""
+	# beta_schedule function
+	def _warmup_beta(linear_start, linear_end, n_timestep, warmup_frac):
+		betas = linear_end * np.ones(n_timestep, dtype=np.float64)
+		warmup_time = int(n_timestep * warmup_frac)
+		betas[:warmup_time] = np.linspace(
+			linear_start, linear_end, warmup_time, dtype=np.float64)
+		return betas
+	if schedule == 'quad':
+		betas = np.linspace(linear_start ** 0.5, linear_end ** 0.5,
+							n_timestep, dtype=np.float64) ** 2
+	elif schedule == 'linear':
+		betas = np.linspace(linear_start, linear_end,
+							n_timestep, dtype=np.float64)
+	elif schedule == 'warmup10':
+		betas = _warmup_beta(linear_start, linear_end,
+							 n_timestep, 0.1)
+	elif schedule == 'warmup50':
+		betas = _warmup_beta(linear_start, linear_end,
+							 n_timestep, 0.5)
+	elif schedule == 'const':
+		betas = linear_end * np.ones(n_timestep, dtype=np.float64)
+	elif schedule == 'jsd':  # 1/T, 1/(T-1), 1/(T-2), ..., 1
+		betas = 1. / np.linspace(n_timestep,
+								 1, n_timestep, dtype=np.float64)
+	elif schedule == "cosine":
+		timesteps = (
+			np.arange(n_timestep + 1, dtype=np.float64) /
+			n_timestep + cosine_s
+		)
+		alphas = timesteps / (1 + cosine_s) * math.pi / 2
+		alphas = np.power(np.cos(alphas),2)
+		alphas = alphas / alphas[0]
+		betas = 1 - alphas[1:] / alphas[:-1]
+		betas = np.clip(betas, 0, 1-1e-3)
+	else:
+		raise NotImplementedError(schedule)
+	return betas
+
+class BaseNetwork(nn.Module):
+	def __init__(self, init_type='kaiming', gain=0.02):
+		super(BaseNetwork, self).__init__()
+		self.init_type = init_type
+		self.gain = gain
+
+	def init_weights(self):
+		"""
+		initialize network's weights
+		init_type: normal | xavier | kaiming | orthogonal
+		https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/9451e70673400885567d08a9e97ade2524c700d0/models/networks.py#L39
+		"""
+	
+		def init_func(m:Union[nn.Module, torch.Tensor]):
+			classname = m.__class__.__name__
+			if classname.find('InstanceNorm2d') != -1:
+				if hasattr(m, 'weight') and m.weight is not None:
+					nn.init.constant_(m.weight.data, 1.0)
+				if hasattr(m, 'bias') and m.bias is not None:
+					nn.init.constant_(m.bias.data, 0.0)
+			elif hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+				if self.init_type == 'normal':
+					nn.init.normal_(m.weight.data, 0.0, self.gain)
+				elif self.init_type == 'xavier':
+					nn.init.xavier_normal_(m.weight.data, gain=self.gain)
+				elif self.init_type == 'xavier_uniform':
+					nn.init.xavier_uniform_(m.weight.data, gain=1.0)
+				elif self.init_type == 'kaiming':
+					nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+				elif self.init_type == 'orthogonal':
+					nn.init.orthogonal_(m.weight.data, gain=self.gain)
+				elif self.init_type == 'none':  # uses pytorch's default init method
+					m.reset_parameters()
+				else:
+					raise NotImplementedError('initialization method [%s] is not implemented' % self.init_type)
+				if hasattr(m, 'bias') and m.bias is not None:
+					nn.init.constant_(m.bias.data, 0.0)
+
+			self.apply(init_func)
+			# propagate to children
+			for m in self.children():
+				if hasattr(m, 'init_weights'):
+					m.init_weights(self.init_type, self.gain)
+
+class Diffuser(BaseNetwork):
+	def __init__(self, denoiser:Denoiser, beta_schedule:Dict, dpm_argv:Dict,  **kwargs):
+		"""Diffuser
+
+		Args:
+			denoiser_paras (dict): hidden_dims:List[int], x_dim:int, depthmap_scale[Optional, float] = 256
+			beta_schedule (dict): schedule:Literal['quad','linear','warmup10','warmup50','const','jsd','cosine'],
+						n_timestep:int, linear_start:float=1e-6, linear_end:float=1e-2, cosine_s:float=8e-3
+			loss_type (Literal['mse','mae']): str
+			noise_std (float): amplitude of noise
+
+		Raises:
+			NotImplementedError: _description_
+		"""
+		super(Diffuser, self).__init__(**kwargs)
+		self.x0_fn = denoiser
+		self.beta_schedule = beta_schedule
+		self.dpm_argv = dpm_argv
+
+	@staticmethod
+	def to_torch(x:Iterable[float], dtype=torch.float32, device=torch.device('cuda')):
+		return torch.tensor(x, dtype=dtype, device=device)
+
+	def set_loss(self, loss_fn):
+		self.loss_fn = loss_fn
+
+	def set_new_noise_schedule(self, device=torch.device('cuda')):
+		to_torch = partial(self.to_torch, dtype=torch.float32, device=device)
+		betas = make_beta_schedule(**self.beta_schedule)
+		# betas = betas.detach().cpu().numpy() if isinstance(
+		#     betas, torch.Tensor) else betas
+		alphas = 1. - betas
+
+		timesteps, = betas.shape
+		self.num_timesteps = int(timesteps)
+		
+		gammas = np.cumprod(alphas, axis=0)
+		gammas_prev = np.append(1., gammas[:-1])
+
+		# calculations for diffusion q(x_t | x_{t-1}) and others
+		self.register_buffer('gammas', to_torch(gammas))
+		self.register_buffer('sqrt_recip_gammas', to_torch(np.sqrt(1. / gammas)))
+		self.register_buffer('sqrt_recipm1_gammas', to_torch(np.sqrt(1. / gammas - 1)))
+		
+		# calculations for posterior q(x_{t-1} | x_t, x_0)
+		posterior_variance = betas * (1. - gammas_prev) / (1. - gammas)
+		# below: log calculation clipped because the posterior variance is 0 at the beginning of the diffusion chain
+		self.register_buffer('posterior_log_variance_clipped', to_torch(np.log(np.maximum(posterior_variance, np.spacing(np.float32(1))))))
+		self.register_buffer('posterior_mean_coef1', to_torch(betas * np.sqrt(gammas_prev) / (1. - gammas)))
+		self.register_buffer('posterior_mean_coef2', to_torch((1. - gammas_prev) * np.sqrt(alphas) / (1. - gammas)))
+
+	def predict_start_from_noise(self, x_t:torch.Tensor, t:torch.Tensor, noise:torch.Tensor):
+		return (
+			extract(self.sqrt_recip_gammas, t, x_t.shape) * x_t -
+			extract(self.sqrt_recipm1_gammas, t, x_t.shape) * noise
+		)
+
+	def q_posterior(self, x_0_hat, x_t, t):
+		posterior_mean = (
+			extract(self.posterior_mean_coef1, t, x_t.shape) * x_0_hat +
+			extract(self.posterior_mean_coef2, t, x_t.shape) * x_t
+		)
+		posterior_log_variance_clipped = extract(self.posterior_log_variance_clipped, t, x_t.shape)
+		return posterior_mean, posterior_log_variance_clipped
+
+	def p_mean_variance(self, x_t:torch.Tensor, t:torch.Tensor, x_cond:torch.Tensor):
+		# sample_gammas = extract(self.gammas, t, x_shape=(1, 1)).to(x_t.device)
+		x_0_hat = self.x0_fn.forward(x_t, x_cond)  # x0 predictor
+		model_mean, posterior_log_variance = self.q_posterior(
+			x_0_hat=x_0_hat, x_t=x_t, t=t)
+		return model_mean, posterior_log_variance
+
+	def q_sample(self, x_0:torch.Tensor, sample_gammas:torch.Tensor, noise=None):
+		noise = default(noise, torch.zeros_like(x_0))
+		return (
+			sample_gammas.sqrt() * x_0 +
+			(1 - sample_gammas).sqrt() * noise
+		)
+
+	@torch.inference_mode()
+	def p_sample(self, x_t, t, x_cond:torch.Tensor):
+		model_mean, model_log_variance = self.p_mean_variance(
+			x_t=x_t, t=t, x_cond=x_cond)
+		noise = torch.zeros_like(x_t) if any(t>0) else torch.zeros_like(x_t)
+		return model_mean + noise * (0.5 * model_log_variance).exp()
+
+	@torch.inference_mode()
+	def ddpm_sampling(self, x_T:torch.Tensor, x_cond:Tuple[torch.Tensor, torch.Tensor, Dict]) -> torch.Tensor:
+		b = x_T.shape[0]
+		x_t = x_T.clone()
+		for i in tqdm(reversed(range(0, self.num_timesteps)), desc='ddpm sampling', total=self.num_timesteps):
+			t = torch.full((b,), i, device=x_t.device, dtype=torch.long)
+			x_t = self.p_sample(x_t, t, x_cond=x_cond)
+		return x_t
+	
+
+	@torch.inference_mode()
+	def dpm_sampling(self, x_T:torch.Tensor, x_cond:Tuple[torch.Tensor, torch.Tensor, Dict]) -> torch.Tensor:
+		def model_fn(x_t, t, x_cond):
+			out = self.x0_fn.forward(x_t, x_cond)
+			# If the model outputs both 'mean' and 'variance' (such as improved-DDPM and guided-diffusion),
+			# We only use the 'mean' output for DPM-Solver, because DPM-Solver is based on diffusion ODEs.
+			return out
+		noise_schedule = NoiseScheduleVP(schedule='discrete', alphas_cumprod=self.gammas)
+		model_fn_continuous = model_wrapper(
+			model_fn,
+			noise_schedule,
+			model_kwargs={"x_cond":x_cond},
+			model_type='x_start',
+			guidance_type='uncond'
+		)
+		dpm_solver = DPM_Solver(
+			model_fn_continuous,
+			noise_schedule,
+			algorithm_type="dpmsolver++"
+		)
+		x_0_hat = dpm_solver.sample(
+			x_T,
+			**self.dpm_argv
+		)
+		return x_0_hat
+
+	def forward(self, x_0:torch.Tensor, x_cond:Tuple[torch.Tensor, torch.Tensor, Dict], noise=None) -> torch.Tensor:
+		"""Training `theta`(x_t, t) = `epsilon_0`
+
+		Args:
+			x_0 (torch.Tensor): gt variable
+			x_cond (Tuple[torch.Tensor]): condition
+			noise (_type_, optional): added Guassian Noise. Defaults to None.
+
+		Returns:
+			loss: error between x0_hat and x0
+		"""
+		b = x_0.shape[0]
+		t = torch.randint(1, self.num_timesteps, (b,), device=x_0.device).long()  # (b,)
+		gamma_t1 = extract(self.gammas, t-1, x_shape=(1, 1))  # (b, 1, 1)
+		sqrt_gamma_t2 = extract(self.gammas, t, x_shape=(1, 1))  # (b, 1, 1)
+		sample_gammas = (sqrt_gamma_t2-gamma_t1) * torch.rand((b, 1), device=x_0.device) + gamma_t1  # randomly linear interpolation between gamma1 and gamm2
+		noise = default(noise, torch.zeros_like(x_0))  # x_T = 0
+		x_t = self.q_sample(
+			x_0=x_0, sample_gammas=sample_gammas.view(b,1), noise=noise)
+		x_0_hat = self.x0_fn.forward(x_t, x_cond)
+		loss = self.loss_fn(x_0_hat, x_0)
+		return loss, x_0_hat
+	
+
