d662cdfec4d99a824a024163f7355eb738f6ed24
gitouni <30369762+gitouni@users.noreply.github.com>
2024-10-16 11:08:31 +0800

    Add files via upload

--- models/denoiser.py
+++ models/denoiser.py
@@ -9,7 +9,7 @@ from .calibnet.CalibNet import CalibNet as VanillaCalibNet
 from .rggnet.rggnet import RGGNet as VanillaRGGNet
 from .lccnet.LCCNet import LCCNet as VanillaLCCNet
 from .lccraft.convgru import LCCRAFT as VanillaLCCRAFT
-from .tools.core import DepthImgGenerator, BasicBlock
+from .tools.core import DepthImgGenerator, BasicBlock, MLPNet
 from .tools.utils import timer
 from functools import partial
 
@@ -111,9 +111,52 @@ class LCCRAFT(Surrogate):
     def clear_buffer(self):
         self.encoder.clear_buffer()
 
+
 class Aggregation(nn.Module):
-    def __init__(self,inplanes:int, planes=96, mlp_dims:Optional[List[int]]=None, final_feat=(2,4), activation_fn:nn.Module=nn.ReLU(inplace=True)):
+    def __init__(self, inplanes=768, planes=96, final_feat=(2,4)):
         super(Aggregation,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=inplanes,out_channels=planes*4,kernel_size=3,stride=1,padding=1)
+        self.bn1 = nn.BatchNorm2d(planes*4)
+        self.conv2 = nn.Conv2d(in_channels=planes*4,out_channels=planes*4,kernel_size=3,stride=1,padding=1)
+        self.bn2 = nn.BatchNorm2d(planes*4)
+        self.conv3 = nn.Conv2d(in_channels=planes*4,out_channels=planes*2,kernel_size=1,stride=1)
+        self.bn3 = nn.BatchNorm2d(planes*2)
+        self.tr_conv = nn.Conv2d(in_channels=planes*2,out_channels=planes,kernel_size=1,stride=1)
+        self.tr_bn = nn.BatchNorm2d(planes)
+        self.rot_conv = nn.Conv2d(in_channels=planes*2,out_channels=planes,kernel_size=1,stride=1)
+        self.rot_bn = nn.BatchNorm2d(planes)
+        self.tr_pool = nn.AdaptiveAvgPool2d(output_size=final_feat)
+        self.rot_pool = nn.AdaptiveAvgPool2d(output_size=final_feat)
+        self.fc1 = nn.Linear(planes*final_feat[0]*final_feat[1],3) 
+        self.fc2 = nn.Linear(planes*final_feat[0]*final_feat[1],3) 
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                nn.init.kaiming_normal_(m.weight.data, mode='fan_in')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+        nn.init.xavier_normal_(self.fc1.weight,0.1)
+        nn.init.xavier_normal_(self.fc2.weight,0.1)
+
+    def forward(self,x:torch.Tensor):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x_tr = self.tr_conv(x)
+        x_tr = self.tr_bn(x_tr)
+        x_tr = self.tr_pool(x_tr)  # (19,6)
+        x_tr = self.fc1(x_tr.view(x_tr.shape[0],-1))
+        x_rot = self.rot_conv(x)
+        x_rot = self.rot_bn(x_rot)
+        x_rot = self.rot_pool(x_rot)  # (19.6)
+        x_rot = self.fc2(x_rot.view(x_rot.shape[0],-1))
+        return torch.cat([x_rot, x_tr],dim=1)
+
+class ResAggregation(nn.Module):
+    def __init__(self,inplanes:int, planes=96, rot_mlp_dims:Optional[List[int]]=None, tsl_mlp_dims:Optional[List[int]]=None, final_feat=(2,4), activation_fn:nn.Module=nn.ReLU(inplace=True)):
+        super(ResAggregation,self).__init__()
         self.head_conv = nn.Sequential(
             BasicBlock(inplanes, planes*4, activation_fnc=activation_fn),
             BasicBlock(planes*4, planes*2, activation_fnc=activation_fn)
@@ -126,18 +169,22 @@ class Aggregation(nn.Module):
             BasicBlock(planes*2, planes, activation_fnc=activation_fn),
             nn.AdaptiveAvgPool2d(output_size=final_feat)
         )
-        if mlp_dims is None:
-            mlp_dims = []
-        mlp_dims = [planes*final_feat[0]*final_feat[1]] + mlp_dims + [3]
+        if rot_mlp_dims is None:
+            rot_mlp_dims = []
+        if tsl_mlp_dims is None:
+            tsl_mlp_dims = []
+        rot_mlp_dims = [planes*final_feat[0]*final_feat[1]] + rot_mlp_dims + [3]
+        tsl_mlp_dims = [planes*final_feat[0]*final_feat[1]] + tsl_mlp_dims + [3]
         rot_fc = []
         tsl_fc = []
-        for i in range(len(mlp_dims) - 2):
-            rot_fc.append(nn.Linear(mlp_dims[i], mlp_dims[i+1]))
+        for i in range(len(rot_mlp_dims) - 2):
+            rot_fc.append(nn.Linear(rot_mlp_dims[i], rot_mlp_dims[i+1]))
             rot_fc.append(activation_fn)
-            tsl_fc.append(nn.Linear(mlp_dims[i], mlp_dims[i+1]))
+        for i in range(len(tsl_mlp_dims) - 2):
+            tsl_fc.append(nn.Linear(tsl_mlp_dims[i], tsl_mlp_dims[i+1]))
             tsl_fc.append(activation_fn)
-        rot_fc.append(nn.Linear(mlp_dims[-2], mlp_dims[-1]))
-        tsl_fc.append(nn.Linear(mlp_dims[-2], mlp_dims[-1]))
+        rot_fc.append(nn.Linear(rot_mlp_dims[-2], rot_mlp_dims[-1]))
+        tsl_fc.append(nn.Linear(tsl_mlp_dims[-2], tsl_mlp_dims[-1]))
         self.rot_fc = nn.Sequential(*rot_fc)
         self.tsl_fc = nn.Sequential(*tsl_fc)
         for m in self.head_conv.modules():
@@ -181,7 +228,7 @@ class ProjFusionNet(Surrogate):
         else:
             fusion_class = FusionNetProjectOnly
         self.encoder = fusion_class(**encoder_argv)
-        self.mlp = Aggregation(inplanes=self.encoder.out_dim, activation_fn=activation_func, **aggregation_argv)
+        self.mlp = ResAggregation(inplanes=self.encoder.out_dim, activation_fn=activation_func, **aggregation_argv)
 
     def forward(self, img:torch.Tensor, pcd:torch.Tensor, Tcl:torch.Tensor, camera_info:Dict):
         feat = self.encoder(img, pcd, Tcl, camera_info)  # (B, D)
